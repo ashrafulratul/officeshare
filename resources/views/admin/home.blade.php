65cc0670395636fc9d48274936de6eb96144cea6
ashrafulratul <softnexttech@gmail.com>
2017-03-19 01:25:13 +0600

    Update Page Left side design

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -18,4 +18,22 @@
         </div>
     </div>
 </div>
+ -->
+
+
+<div class="Leftpanel">
+<div class="row">
+        <div class="panel-heading">Dashboard</div>
+
+        <div class="panel-body" ng-controller="TestController">
+            You  @{{ name }} are logged in!
+            <input type="text" ng-model="my" />
+            <p>@{{ my }}</p>
+
+        </div>
+
+</div>
+</div>
+
+<div id="LeftpanelBG"></div>
 @endsection
